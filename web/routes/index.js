dca40a03bdd2e5d4e9bcb9e7d83efcdd17f07c13
Pawan Sunuwar <pawanman56@gmail.com>
2024-10-31 12:00:08 +0000

    add docker service orchestration

--- web/routes/index.js
+++ web/routes/index.js
@@ -13,9 +13,9 @@ router.get('/', function(req, res, next) {
         // get top songs
         console.log('Artist is ' + artist);
 
-        rest.get('http://search:8080/api/artists/search?artist=' + artist).on('complete', function(data) {
+        rest.get('http://localhost:8080/api/artists/search?artist=' + artist).on('complete', function(data) {
           console.log('Artist ID is ' + data["id"]);
-          rest.get('http://charts:8080/api/charts/' + data["id"]).on('complete', function(data) {
+          rest.get('http://localhost:8080/api/charts/' + data["id"]).on('complete', function(data) {
             console.log('Top songs are ' + JSON.stringify(data));
             callback(null, data);
           });
@@ -25,10 +25,10 @@ router.get('/', function(req, res, next) {
           // get cover data
           console.log('Title is ' + title);
 
-          rest.get('http://search:8080/api/tracks/search?title=' + title + '&artist=' + artist).on('complete', function(data) {
+          rest.get('http://localhost:8080/api/tracks/search?title=' + title + '&artist=' + artist).on('complete', function(data) {
             console.log('Title ID is ' + data["id"]);
 
-            rest.get('http://images:8080/api/covers/' + data["id"]).on('complete', function(data) {
+            rest.get('http://localhost:8080/api/covers/' + data["id"]).on('complete', function(data) {
               console.log('Cover image is ' + data["url"]);
               callback(null, data["url"]);
             });
